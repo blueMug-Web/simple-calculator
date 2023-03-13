974c261ac6612865a23fab6053747deb38dbd37d
blueMug <wrightman.kyle@gmail.com>
2023-03-13 13:21:32 -0400

    First full commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const inputRef = useRef(null);
+	const resultRef = useRef(null);
+	const [result, setResult] = useState(0);
+
+	const plus = (e) => {
+		e.preventDefault();
+		setResult((result) => result + Number(inputRef.current.value));
+	};
+	const minus = (e) => {
+		e.preventDefault();
+		setResult((result) => result - Number(inputRef.current.value));
+	};
+	const times = (e) => {
+		e.preventDefault();
+		setResult((result) => result * Number(inputRef.current.value));
+	};
+	const divide = (e) => {
+		e.preventDefault();
+		setResult((result) => result / Number(inputRef.current.value));
+	};
+
+	const resetInput = (e) => {
+		e.preventDefault();
+		inputRef.current.value = 0;
+	};
+	const resetResult = (e) => {
+		e.preventDefault();
+		setResult((prevVal) => prevVal * 0);
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h1>A Simple Calcultor</h1>
+			</div>
+			<form>
+				<p ref={resultRef}>{result}</p>
+				<input
+					className="input"
+					pattern="[0-9]"
+					ref={inputRef}
+					type="number"
+					placeholder="enter a number"
+				/>
+				<button className="button" onClick={plus}>
+					add
+				</button>
+				<button className="button" onClick={minus}>
+					subtract
+				</button>
+				<button className="button" onClick={times}>
+					multiply
+				</button>
+				<button className="button" onClick={divide}>
+					divide
+				</button>
+				<div className="reset-buttons">
+					<button id="reset" onClick={resetInput}>
+						reset input
+					</button>
+					<button id="reset" onClick={resetResult}>
+						reset result
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
